8d9de62e0c8a35bce9917a5a75b1767cc7c0169b
Babi-B <babibyoolah>
2023-08-31 14:45:43 +0100

    Built the app file to import and use the other modules

--- app.py
+++ app.py
@@ -1,1 +1,51 @@
-print("Hello, world!")
+from time import sleep
+from intro import intro
+from create import create
+from display import display
+from delete_todo import delete_todo
+
+def app():
+    runApp = True
+
+    while runApp:
+            # run the intro screen
+        num = intro()
+
+        # Check if num is a numeric
+        while not num.isnumeric():
+            print("")
+            num = input("PLEASE ENTER A NUMBER: ")
+
+        # Convert num to an integer
+        userInput = int(num)
+
+        match userInput:
+            case 1:
+                create()
+                sleep(1)
+            case 2:
+                display()
+                print("Back to the main menu")
+                sleep(1)
+            case 3:
+                delete_todo()
+                sleep(1)
+            case 4:
+                create()
+            case 5:
+                print("")
+                print("Thank you for using this app!")
+                print("")
+                sleep(1)
+                runApp = False
+            case _:
+                print("")
+                print("Invalid input!")
+                sleep(.2)
+                print("")
+                print("")
+                print("            Program shutting down...")
+                print("")
+                print("")
+                sleep(2)
+app()
