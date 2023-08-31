74124847f20dd4fe329473b8af0cef748f4f65f0
Babi-B <babibyoolah>
2023-08-31 14:45:11 +0100

    Refactored the intro file

--- intro.py
+++ intro.py
@@ -1,44 +1,27 @@
 from time import sleep
-from create import create
 
-print("******************************************************")
-print("")
-print("               WELCOME ")
-sleep(.2)
-print("")
-print("What do you wish to do? (Please enter a number)")
-print("")
-print("1. Add a to-do")
-print("2. View all your to-do's")
-print("3. Mark off a to-do")
-print("")
-sleep(.2)
-print("")
-
-num = input("Please enter the corresponding number: ")
-while not num.isnumeric():
+def intro():
+    print("******************************************************")
     print("")
-    num = input("PLEASE ENTER A NUMBER: ")
+    print("               WELCOME ")
+    sleep(.2)
+    print("")
+    print("What do you wish to do? (Please enter a number)")
+    print("")
+    print("1. Add a to-do")
+    print("2. View all your to-do's")
+    print("3. Mark off a to-do")
+    print("4. See tasks you've completed")
+    print("5. Quit")
+    print("")
+    sleep(.2)
+    print("")
 
-userInput = int(num)
+    num = input("Please enter the corresponding number: ")
+    
+    return num
 
-match userInput:
-    case 1:
-        create()
-    case 2:
-        create()
-    case 3:
-        create()
-    case _:
-        print("")
-        print("Invalid input!")
-        sleep(.2)
-        print("")
-        print("")
-        print("            Program shutting down...")
-        print("")
-        print("")
-        sleep(2)
+
 
 
     
